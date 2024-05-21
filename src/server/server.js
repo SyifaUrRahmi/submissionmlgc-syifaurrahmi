31f67e878dfe2f97683a585204192d71f215400c
SyifaUrRahmi <sifasisi3@gmail.com>
2024-05-21 14:13:44 +0800

    Push code to remote repository

---
+++ src/server/server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+const Hapi = require("@hapi/hapi");
+const routes = require("../server/routes");
+const loadModel = require("../services/loadModel");
+
+(async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  const model = await loadModel();
+  server.app.model = model;
+
+  server.route(routes);
+
+  server.ext("onPreResponse", function (request, h) {
+    const response = request.response;
+
+    if (response.isBoom) {
+      const statusCode = Number.isInteger(response.output.statusCode)
+        ? response.output.statusCode
+        : 500;
+      const newResponse = h.response({
+        status: "fail",
+        message: response.message,
+      });
+      newResponse.code(statusCode);
+      return newResponse;
+    }
+    return h.continue;
+  });
+
+  await server.start();
+  console.log(`Server start at: ${server.info.uri}`);
+})();
