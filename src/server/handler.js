ed1446cc562404139b9ba65576ab14bef9a10dd4
SyifaUrRahmi <sifasisi3@gmail.com>
2024-05-21 23:23:00 +0800

    menyimpan data di firestore

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,35 +1,7 @@
-// const predictClassification = require("../services/inferenceService");
-// const crypto = require("crypto");
-
-// async function postPredictHandler(request, h) {
-//   const { image } = request.payload;
-//   const { model } = request.server.app;
-
-//   const { label, suggestion } = await predictClassification(model, image);
-
-//   const id = crypto.randomUUID();
-//   const createdAt = new Date().toISOString();
-
-//   const data = {
-//     result: label,
-//     createdAt: createdAt,
-//     suggestion: suggestion,
-//     id: id,
-//   };
-
-//   const response = h.response({
-//     status: "success",
-//     message: "Model is predicted successfully.",
-//     data,
-//   });
-//   response.code(201);
-//   return response;
-// }
-
-// module.exports = postPredictHandler;
 const Boom = require("@hapi/boom");
 const predictClassification = require("../services/inferenceService");
 const crypto = require("crypto");
+const storeData = require("../services/storeData");
 
 const postPredictHandler = async (request, h) => {
   try {
@@ -55,18 +27,16 @@ const postPredictHandler = async (request, h) => {
       id: id,
     };
 
+    await storeData(id, data);
     return {
       status: "success",
       message: "Model is predicted successfully",
       data: data,
     };
   } catch (error) {
-    // Tangani kesalahan dari model prediksi atau kesalahan lainnya
     if (Boom.isBoom(error)) {
-      // Jika kesalahan berasal dari Boom, langsung kembalikan respons error yang dibuat oleh Boom
       throw error;
     } else {
-      // Jika kesalahan bukan berasal dari Boom, tangani dengan pesan kesalahan yang sesuai
       throw Boom.badRequest("Terjadi kesalahan dalam melakukan prediksi");
     }
   }
